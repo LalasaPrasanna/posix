478cd600e2e1134cd334705e2d792f56dc2407ae
LalasaPrasanna <167599797+LalasaPrasanna@users.noreply.github.com>
2026-02-05 14:56:57 +0000

    Commit

---
+++ mutex.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <pthread.h>
+
+int counter = 0;
+pthread_mutex_t mutex;
+
+void* thread(void* arg) {
+    for (int i = 0; i < 100; i++) {
+        pthread_mutex_lock(&mutex);
+        counter++;
+        pthread_mutex_unlock(&mutex);
+    }
+    return NULL;
+}
+
+int main() {
+    pthread_t t1, t2;
+    pthread_mutex_init(&mutex, NULL);
+    pthread_create(&t1, NULL, thread, NULL);
+    pthread_join(t1, NULL);
+
+    pthread_mutex_destroy(&mutex);
+
+    printf("%d\n", counter);
+    return 0;
+}
